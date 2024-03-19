7c22b364726a54357d8898100a3d41f8f29bf4ea
Raghunandan Bansal <raghubansal1102@gmail.com>
2024-03-19 20:15:02 +0530

    main kotlin file updated

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,92 @@
 package com.example.weatherapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    val city: String= "Kanpur"
+    val API: String= "e0c99aa244528a93f943720d430323c7"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
+    }
+
+    inner class weatherTask(): AsyncTask<String,Unit, String>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility= View.VISIBLE
+            findViewById<RelativeLayout>(R.id.mainContainer).visibility= View.GONE
+            findViewById<TextView>(R.id.errortext).visibility= View.GONE
+        }
+
+        override fun doInBackground(vararg p0: String?): String? {
+            var response: String?
+            try{
+                response=  URL("https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$API")
+                    .readText(Charsets.UTF_8)
+            }
+            catch (e: Exception){
+                response= null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try{
+                val jsonObj= JSONObject(result)
+                val main= jsonObj.getJSONObject("main")
+                val sys= jsonObj.getJSONObject("sys")
+                val wind= jsonObj.getJSONObject("wind")
+                val weather= jsonObj.getJSONArray("weather").getJSONObject(0)
+                val updateAt: Long= jsonObj.getLong("dt")
+                val updateTextAt= "update at : " + SimpleDateFormat("dd/mm/yyyy hh:mm a", Locale.ENGLISH).format(
+                    Date(updateAt*1000)
+                )
+
+                val temp= main.getString("temp") + "°C"
+                val tempMin= "Min Temp"+ main.getString("temp_min")+ "°C"
+                val tempMax= "Max Temp"+ main.getString("temp_max")+ "°C"
+                val pressure= main.getString("pressure")
+                val humidity= main.getString("humidity")
+                val sunrise:Long= sys.getLong("sunrise")
+                val sunset:Long= sys.getLong("sunset")
+                val windspeed= wind.getString("speed")
+                val weatherDescritpion= weather.getString("description")
+                val address= jsonObj.getString("name")+ ","+ sys.getString("country")
+
+                findViewById<TextView>(R.id.address).text= address
+                findViewById<TextView>(R.id.updated).text= updateTextAt
+                findViewById<TextView>(R.id.status).text= weatherDescritpion.capitalize()
+                findViewById<TextView>(R.id.temp).text= temp
+                findViewById<TextView>(R.id.temp_min).text= tempMin
+                findViewById<TextView>(R.id.temp_max).text= tempMax
+                findViewById<TextView>(R.id.sunrise).text= SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                findViewById<TextView>(R.id.sunset).text= SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.wind).text= windspeed
+                findViewById<TextView>(R.id.pressure).text= pressure
+                findViewById<TextView>(R.id.humidity).text= humidity
+
+                findViewById<ProgressBar>(R.id.loader).visibility= View.GONE
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility= View.VISIBLE
+            }
+            catch (e:Exception){
+                findViewById<ProgressBar>(R.id.loader).visibility= View.GONE
+                findViewById<TextView>(R.id.errortext).visibility= View.VISIBLE
+            }
+        }
     }
 }
